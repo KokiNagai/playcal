4894753379d4d32f30dc34093b9e045dff2592a4
KokiNagai <kokinagai10@gmail.com>
2018-10-05 16:33:21 +0900

    17

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,15 @@
 require_relative 'boot'
+require 'rails/all'
+require 'carrierwave'
+require 'devise'
+require 'carrierwave/orm/activerecord'
+
 
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 
 module Tennis
@@ -15,12 +21,6 @@ module Tennis
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.assets.initialize_on_precompile = false
-
-    require 'rails/all'
-    require 'carrierwave'
-    require 'devise'
-    require 'carrierwave/orm/activerecord'
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
