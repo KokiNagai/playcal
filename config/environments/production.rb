c4dd8eb01207d739682be10f1bde776ab4bbf859
KokiNagai <kokinagai10@gmail.com>
2018-10-05 14:32:02 +0900

    9th

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,9 +70,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :user_name => "playcalinfo@gmail.com",
-      :password => "SendGridのパスワード",
+      :password => "gqkoxeulgztoiodh",
       :domain => "heroku.com",
-      :address => "gqkoxeulgztoiodh",
+      :address => "smtp.gmail.com",
       :port => 587,
       :authentication => :plain,
       :enable_starttls_auto => true
