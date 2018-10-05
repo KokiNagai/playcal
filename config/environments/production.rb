8f10eb3bc2108505f6edcf4a0ec19e4df737fee8
KokiNagai <kokinagai10@gmail.com>
2018-10-05 14:29:04 +0900

    8th

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {host: 'https://playcal.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :user_name => "playcalinfo@gmail.com",
+      :password => "SendGridのパスワード",
+      :domain => "heroku.com",
+      :address => "gqkoxeulgztoiodh",
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+end
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
