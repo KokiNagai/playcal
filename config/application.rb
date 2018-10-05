f40c9b886ac5ec4bd60c0522563ff8bf54f17acd
KokiNagai <kokinagai10@gmail.com>
2018-10-05 14:34:15 +0900

    10th

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Tennis
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
