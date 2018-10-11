9d401d4ae7fceb6e291aef521bd353c32dc80356
KokiNagai <kokinagai10@gmail.com>
2018-10-11 23:54:29 +0900

    f

--- app/assets/javascripts/notify.js
+++ app/assets/javascripts/notify.js
@@ -2,7 +2,7 @@ $(function() {
   $('.JS-bell').click(function() {
     $('.notification-wrapper').fadeIn();
   });
-  $('#close-cross').click(function() {
+  $('.close-cross').click(function() {
     $('.notification-wrapper').fadeOut();
   });
 });
