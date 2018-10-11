2a51ed8cc35310c51bf1c34948b1a4d8dbfe7150
KokiNagai <kokinagai10@gmail.com>
2018-10-11 23:36:06 +0900

    f

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: 'AKIAJNXQBHRWHLT4ZKAA',
-    aws_secret_access_key: '6IpVw4yaWcVpNbEho/2/g4UUNLXYGJjJKaI+ZQz',
+    aws_secret_access_key: '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',
     region: 'ap-northeast-1'
   }
 
