9962d5f430163f5f32af4b3937ab5f24a76aad13
KokiNagai <kokinagai10@gmail.com>
2018-10-18 16:38:10 +0900

    fx

--- db/migrate/20181017145904_add_touropen_to_chatboxes.rb
+++ db/migrate/20181017145904_add_touropen_to_chatboxes.rb
@@ -1,5 +1,5 @@
 class AddTouropenToChatboxes < ActiveRecord::Migration[5.2]
   def change
-    add_column :chatboxes, :touropen, :boolean, default: false, null: false
+    add_column :chatboxes, :touropen, :boolean, default: true, null: false
   end
 end
