eb0986fc623c8756e3a5c4619d2269b53d513101
KokiNagai <kokinagai10@gmail.com>
2018-10-21 01:13:43 +0900

    fx

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,6 @@ class NotificationsController < ApplicationController
   def link_through
     @notifications = current_user.notifications
     @notifications.update read: true
-    redirect_to inbox_path
+    redirect_to message_path
   end
 end
