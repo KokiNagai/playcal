3c4ee6930d01146491be002e540e52be56964455
KokiNagai <kokinagai10@gmail.com>
2018-10-27 20:30:35 +0900

    fx

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Tennis
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.assets.initialize_on_precompile = false
     config.action_view.field_error_proc = proc { |html_tag, _| html_tag }
+    config.enable_dependency_loading = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
