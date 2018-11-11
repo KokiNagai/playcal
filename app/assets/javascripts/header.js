8dd0e7f42596b663a90863a87a2598c930c1f1f3
KokiNagai <kokinagai10@gmail.com>
2018-11-12 00:21:25 +0900

    fx

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,9 @@
+function headerListOver(number) {
+  // document.getElementById('header-lists-' + number).style.borderBottom = "3px solid #9cd75b";
+  document.getElementById('header-lists-' + number).style.background = "#f7f7f7";
+}
+
+function headerListOut(number) {
+  // document.getElementById('header-lists-' + number).style.borderBottom = "";
+  document.getElementById('header-lists-' + number).style.background = "";
+}
