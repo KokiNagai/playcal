c515361c47d668cd08adc1dc7a562f1cb37ded8d
KokiNagai <kokinagai10@gmail.com>
2018-11-16 22:33:57 +0900

    fx

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,6 +51,7 @@ end
       twitter: {
         card: 'summary',
         site: '@playcal_official',
+        image: image_url("https://s3-ap-northeast-1.amazonaws.com/playcal-picture/playcal/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88+2018-10-13+12.33.54.png"),
       }
     }
   end
