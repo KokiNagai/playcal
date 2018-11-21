26e64c7365bf7a9859c0147ab5220142a86e37b7
KokiNagai <kokinagai10@gmail.com>
2018-11-21 18:29:54 +0900

    search pref

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+
+  def prefURL(name)
+    prefURL = "search?utf8=✓&q%5Bprefecture_eq%5D=" + name + "&q%5Bcity_cont%5D=&q%5Bplayday_gteq%281i%29%5D=&q%5Bplayday_gteq%282i%29%5D=&q%5Bplayday_gteq%283i%29%5D=&q%5Bplayday_lteq%281i%29%5D=&q%5Bplayday_lteq%282i%29%5D=&q%5Bplayday_lteq%283i%29%5D=&q%5Bstyle_eq%5D=&button="
+  end
 end
