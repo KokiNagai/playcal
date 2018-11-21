57908086fc5be5936d1c74912e09fbe33f5e8a1b
KokiNagai <kokinagai10@gmail.com>
2018-11-21 21:53:02 +0900

    Fx

--- app/assets/javascripts/find.js
+++ app/assets/javascripts/find.js
@@ -1,51 +1,60 @@
 $(function(){
 	$('.pref-btn1').on('click', function(){
 		$('.find-pref-button1').toggleClass('dropdown_toggle').children('.find-pref-1').slideToggle(400);
+    $('.find-pref-button1').toggleClass('find-pref-btn-shadow');
 	});
 });
 
 $(function(){
 	$('.pref-btn2').on('click', function(){
 		$('.find-pref-button2').toggleClass('dropdown_toggle').children('.find-pref-2').slideToggle(400);
+    $('.find-pref-button2').toggleClass('find-pref-btn-shadow');
 	});
 });
 
 $(function(){
 	$('.pref-btn3').on('click', function(){
 		$('.find-pref-button3').toggleClass('dropdown_toggle').children('.find-pref-3').slideToggle(400);
+    $('.find-pref-button3').toggleClass('find-pref-btn-shadow');
 	});
 });
 
 $(function(){
 	$('.pref-btn4').on('click', function(){
 		$('.find-pref-button4').toggleClass('dropdown_toggle').children('.find-pref-4').slideToggle(400);
+    $('.find-pref-button4').toggleClass('find-pref-btn-shadow');
 	});
 });
 
 $(function(){
 	$('.pref-btn5').on('click', function(){
 		$('.find-pref-button5').toggleClass('dropdown_toggle').children('.find-pref-5').slideToggle(400);
+    $('.find-pref-button5').toggleClass('find-pref-btn-shadow');
 	});
 });
 
 $(function(){
 	$('.pref-btn6').on('click', function(){
 		$('.find-pref-button6').toggleClass('dropdown_toggle').children('.find-pref-6').slideToggle(400);
+    $('.find-pref-button6').toggleClass('find-pref-btn-shadow');
 	});
 });
 
 $(function(){
 	$('.pref-btn7').on('click', function(){
 		$('.find-pref-button7').toggleClass('dropdown_toggle').children('.find-pref-7').slideToggle(400);
+    $('.find-pref-button7').toggleClass('find-pref-btn-shadow');
 	});
 });
 $(function(){
 	$('.pref-btn8').on('click', function(){
 		$('.find-pref-button8').toggleClass('dropdown_toggle').children('.find-pref-8').slideToggle(400);
+    $('.find-pref-button9').toggleClass('find-pref-btn-shadow');
 	});
 });
 $(function(){
 	$('.pref-btn9').on('click', function(){
 		$('.find-pref-button9').toggleClass('dropdown_toggle').children('.find-pref-9').slideToggle(400);
+    $('.find-pref-button9').toggleClass('find-pref-btn-shadow');
 	});
 });
