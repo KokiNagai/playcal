84b5d70303fd733b0a40cf9272d40865cd5fcf56
KokiNagai <kokinagai10@gmail.com>
2018-11-23 15:37:55 +0900

    fx

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "gmail.com",
     :user_name => "playcalinfo@gmail.com",
     :password => "gqkoxeulgztoiodh",
     :authentication => :plain,
