20ec004ba7b806e0b10bfd711e1ef089a15b8a7f
KokiNagai <kokinagai10@gmail.com>
2018-11-25 17:51:25 +0900

    fx

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -20,27 +20,14 @@ $(function() {
   });
 });
 
-
-$(function(){
-
-  $(document).on('click', function(e) {
-    // ２．クリックされた場所の判定
-    if(!$(e.target).closest('.search-modal').length && !$(e.target).closest('.gender-JS').length){
-        $('.search-modal-wrapper').fadeOut();
-    } else if($(e.target).closest('.gender-JS').length){
-        // ３．ポップアップの表示状態の判定
-        if($('.search-modal-wrapper').is(':hidden')){
-            $('.search-modal-wrapper').fadeIn();
-        }else{
-            $('.search-modal-wrapper').fadeOut();
-        }
-    }
-});
-
-$('.close-modal2').fastClick(function() {
-  $('.search-modal-wrapper').fadeOut();
-});
-});
+$(function() {
+  $('.gender-JS').on('click', function() {
+    $('.search-modal-wrapper').toggleClass('search-modal-display').fadeIn(400);
+  })
+  $('i#close-modal').on('click', function() {
+    $('.search-modal-wrapper').toggleClass('search-modal-display').fadeOut(400);
+  })
+})
 
 $(function() {
   $('.user-post-recrut').fastClick(function() {
