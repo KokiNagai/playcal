7a1ee3c5780c8013f1f57084a4ab39ae0870c870
KokiNagai <kokinagai10@gmail.com>
2018-11-27 00:24:13 +0900

    fx

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -87,6 +87,20 @@ $(function() {
 })
 
 
+$(function() {
+  $('#post-menu').on('click', function() {
+    $('.post-menu-list').fadeToggle(400);
+  })
+})
+
+
+$(function() {
+  $('#post-menu1').on('click', function() {
+    $('.post-menu-list1').fadeToggle(400);
+  })
+})
+
+
 // $(function() {
 //      $('#edit-file-input').css({
 //          'position': 'absolute',
