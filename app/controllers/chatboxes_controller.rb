14d97fbb50ee20994625ca8f9d56ece67b0d27d4
KokiNagai <kokinagai10@gmail.com>
2019-01-30 17:59:15 +0900

    fx

--- app/controllers/chatboxes_controller.rb
+++ app/controllers/chatboxes_controller.rb
@@ -2,11 +2,13 @@ class ChatboxesController < ApplicationController
   before_action :new_user
 
   def chatbox
-    @chatbox = Chatbox.find_by(chatname: params[:chatname])
+    @chatbox = Chatbox.find_by(id: params[:id])
+    if @chatbox.present?
     @chats = Chat.where(chatbox_id: @chatbox.id).order(created_at: :ASC)
     if user_signed_in?
     @chat = current_user.chats.build(chatbox_id: @chatbox.id, user_id: current_user.id)
   end
+end
 end
 
 private
